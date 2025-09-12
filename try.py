3fbfc734e9c1112d7a904f1b91efdf213a53c13c
venu Bora <chinnavenu190@gmail.com>
2025-09-12 11:44:10 -1000

    htmltags

---
+++ try.py
@@ -0,0 +1,11 @@
+# first = input()
+# second = float(input())
+# third = int(input())
+
+# print(int(first) +  second + third)
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+print(num1 * num2 * num3)
